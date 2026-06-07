c0c46ce69a06f9df5df3bc2be6d7a66585fe1c92
Rafa <150351653+Rafa00127@users.noreply.github.com>
2026-06-07 12:18:19 +0800

    Revert "Codec uses gpu for convtranspose1d"
    This reverts commit 9bffce13551cf8162a910a06dd14362f6864ea5d.

--- Backend/ggml/src/ggml-cuda/conv-transpose-1d.cu
+++ Backend/ggml/src/ggml-cuda/conv-transpose-1d.cu
@@ -1,55 +1,54 @@
 #include "conv-transpose-1d.cuh"
 #include "convert.cuh"
-#include <algorithm>
 
 template <typename src0_t>
 static __global__ void conv_transpose_1d_kernel(
-        const int s0, const int p0, const int d0, const int64_t output_size,
+        const int s0, const int p0, const int d0, const int output_size,
         const int src0_ne0, const int src0_ne1, const int src0_ne2, const int src0_ne3,
         const int src1_ne0, const int src1_ne1, const int src1_ne2, const int src1_ne3,
         const int dst_ne0, const int dst_ne1, const int dst_ne2, const int dst_ne3,
         const src0_t * src0, const float * src1,  float * dst) {
-
-    for (int64_t global_index = threadIdx.x + blockIdx.x * blockDim.x; global_index < output_size; global_index += blockDim.x * gridDim.x) {
-        int out_index = global_index / dst_ne0;
-
-        float accumulator = 0;
-
-        for (int c = 0; c < src0_ne2; c++) {
-            int idx = global_index % dst_ne0;
-
-            int kernel_offset = (src0_ne0 * src0_ne1 * c) + (out_index * src0_ne0);
-            int input_offset = src1_ne0 * c;
-
-            int start_i = (idx >= src0_ne0) ? (idx - src0_ne0 + s0) / s0 : 0;
-            int end_i = idx / s0;
-
-            if (end_i >= src1_ne0) end_i = src1_ne0 - 1;
-
-            for (int i = start_i; i <= end_i; i++) {
-                int weight_idx = idx - i*s0;
-                float kernel_weight = ggml_cuda_cast<float>(src0[kernel_offset + weight_idx]);
-                float input_value =  src1[input_offset+i];
-
-                accumulator += kernel_weight * input_value;
-            }
-        }
-        dst[global_index] = accumulator;
+    int global_index = threadIdx.x + blockIdx.x * blockDim.x;
+    if (global_index >= output_size) {
+        return;
     }
+
+    int out_index = global_index / dst_ne0;
+
+    float accumulator = 0;
+
+    for (int c = 0; c < src0_ne2; c++) {
+        int idx = global_index % dst_ne0;
+
+        int kernel_offset = (src0_ne0 * src0_ne1 * c) + (out_index * src0_ne0);
+        int input_offset = src1_ne0 * c;
+
+        for (int i = 0; i < src1_ne0; i++) {
+            if (!(idx >= i*s0 && idx < i*s0 + src0_ne0)) {
+                continue;
+            }
+            int weight_idx = idx - i*s0;
+
+            float kernel_weight = ggml_cuda_cast<float>(src0[kernel_offset + weight_idx]);
+            float input_value =  src1[input_offset+i];
+
+            accumulator += kernel_weight * input_value;
+        }
+    }
+    dst[global_index] = accumulator;
     GGML_UNUSED_VARS(p0, d0, src0_ne3, src1_ne3, dst_ne3, src1_ne1, dst_ne1, src1_ne2, dst_ne2);
 }
 
 template <typename src0_t>
 static void conv_transpose_1d_cuda(
-        const int s0, const int p0, const int d0, const int64_t output_size,
+        const int s0, const int p0, const int d0, const int output_size,
         const int src0_ne0, const int src0_ne1, const int src0_ne2, const int src0_ne3,
         const int src1_ne0, const int src1_ne1, const int src1_ne2, const int src1_ne3,
         const int dst_ne0, const int dst_ne1, const int dst_ne2, const int dst_ne3,
         const src0_t * src0, const float * src1,  float * dst,
         cudaStream_t stream) {
 
-    const int num_blocks = std::min((int64_t)65535, (output_size + CUDA_CONV_TRANPOSE_1D_BLOCK_SIZE - 1) / CUDA_CONV_TRANPOSE_1D_BLOCK_SIZE);
-
+    const int num_blocks = (output_size + CUDA_CONV_TRANPOSE_1D_BLOCK_SIZE - 1) / CUDA_CONV_TRANPOSE_1D_BLOCK_SIZE;
     conv_transpose_1d_kernel<<<num_blocks,CUDA_CONV_TRANPOSE_1D_BLOCK_SIZE, 0, stream>>>(
         s0,p0,d0,output_size,
         src0_ne0, src0_ne1,  src0_ne2, src0_ne3,
